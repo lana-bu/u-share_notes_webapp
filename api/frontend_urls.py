ee64109ce355de4ff27d1ede96dfb568d493a8f0
lana-bu <lanabu@umich.edu>
2026-04-13 13:51:45 -0400

    undo of first merge of login/signup branch

--- api/frontend_urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import LoginPageView, LogoutPageView, SignupPageView, PostsPageView
-
-urlpatterns = [
-    path('login/', LoginPageView.as_view(), name='login_page'),
-    path('logout/', LogoutPageView.as_view(), name='logout_page'),
-    path('signup/', SignupPageView.as_view(), name='signup_page'),
-    path('posts/', PostsPageView.as_view(), name='posts_page'),
-]
