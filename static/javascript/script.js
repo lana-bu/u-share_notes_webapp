6f8a674b273a6e719a99b8a8e621ba67bcfa19cd
lana-bu <lanabu@umich.edu>
2026-04-14 02:50:45 -0400

    nav sidebar responsive design

--- static/javascript/script.js
+++ static/javascript/script.js
@@ -5,12 +5,13 @@ if (document.readyState == 'loading') {
 }
 
 function ready() {
-    var menuButton = document.getElementById('menu-btn');
+    const menuButton = document.getElementById('menu-btn');
     menuButton.addEventListener('click', () => toggleSidebar());
 }
 
 function toggleSidebar() {
-    var sidebar = document.getElementById("nav-sidebar");
+    const sidebar = document.getElementById("nav-sidebar");
+
     if (sidebar.style.display == "block") {
         sidebar.style.display = "none";
     } else {
