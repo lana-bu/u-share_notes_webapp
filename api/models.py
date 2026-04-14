13812884fc13549e216e7e7b68dc50a54d52a534
lana-bu <lanabu@umich.edu>
2026-04-14 13:07:07 -0400

    modified TODO comments

--- api/models.py
+++ api/models.py
@@ -29,5 +29,5 @@ class Post(models.Model):
     def __str__(self):
         return f"{self.title} - {self.course_number}"
     
-    # TODO: add validateInput and createPost functions
-    # TODO: maybe create get functions for unit testing, unless we can just access like how we are in templates
+    # TODO: add validateInput, createPost, updatePost, and deletePost functions
+    # make sure we can get data similarly to templates with Post.objects.all()
